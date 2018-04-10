82b3d040e2111ade5353ada47dff3d99c3353b17
Aaron Nielsen <apn@apnielsen.com>
2018-04-10 05:22:47 -0400

    added README, controllers and start-minecraft script

---
+++ start-minecraft.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# start-minecraft.sh
+#
+# wrap minecraft in a startup script that will also start xboxdrv
+# to allow for use of a controller.
+
+sudo /usr/bin/xboxdrv -c $HOME/minecraft/xboxdrv/xboxdrv.config/usb-Xiaoji_Gamesir-G3w-event-joystick.xboxdrv -c $HOME/minecraft/xboxdrv/xboxdrv.config/Minecraft.xboxdrv &
+XBOXDRV_PID=$!
+
+java -jar $HOME/minecraft/Minecraft.jar
+
+# kill $XBOXDRV_PID
+
+sudo killall xboxdrv
